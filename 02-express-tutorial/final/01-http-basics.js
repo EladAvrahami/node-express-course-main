fda9c74a94aade92cab753294c7de144401f9907
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2022-10-12 13:27:39 +0300

    comma

--- 02-express-tutorial/final/01-http-basics.js
+++ 02-express-tutorial/final/01-http-basics.js
@@ -2,6 +2,7 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
   // console.log(req.method)
+  // console.log(req.url)  
   const url = req.url
   // home page
   if (url === '/') {
